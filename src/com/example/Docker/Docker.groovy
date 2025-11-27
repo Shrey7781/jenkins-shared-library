1776a688341858447143b96bb4042885af7f234f
Shrey7781 <shreeyansh.saxena.dev@gmail.com>
2025-11-27 13:33:19 +0530

    Update Docker.groovy

--- src/com/example/Docker/Docker.groovy
+++ src/com/example/Docker/Docker.groovy
@@ -1,7 +1,8 @@
 package com.example
-#!usr/bin/env groovy
+#!/usr/bin/env groovy
 
 class Docker implements Serializable {
+
     def script
 
     Docker(script) {
@@ -9,11 +10,24 @@ class Docker implements Serializable {
     }
 
     def buildDockerImage(String imageName) {
+
         script.echo "building the docker image..."
-        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-            script.sh "docker build -t shrey7781/$imageName ."
-            script.sh "echo '${script.PASS}' | docker login -u '${script.USER}' --password-stdin"
-            script.sh "docker push shrey7781/$imageName"
+
+        script.withCredentials([
+            script.usernamePassword(
+                credentialsId: 'docker-hub-repo',
+                passwordVariable: 'PASS',
+                usernameVariable: 'USER'
+            )
+        ]) {
+
+            
+            script.sh "docker build -t ${imageName} ."
+
+            script.sh 'echo $PASS | docker login -u $USER --password-stdin'
+
+           
+            script.sh "docker push ${imageName}"
         }
     }
 }
