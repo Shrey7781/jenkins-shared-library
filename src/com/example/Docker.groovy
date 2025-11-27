0f4c1a70cef52428f8a85795c69653ec70e1e956
Shrey7781 <shreeyansh.saxena.dev@gmail.com>
2025-11-27 13:50:59 +0530

    adding functions in Docker class

---
+++ src/com/example/Docker.groovy
@@ -0,0 +1,19 @@
+package com.example
+#!usr/bin/env groovy
+
+class Docker implements Serializable {
+    def script
+
+    Docker(script) {
+        this.script = script
+    }
+
+    def buildDockerImage(String imageName) {
+        script.echo "building the docker image..."
+        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+            script.sh "docker build -t shrey7781/$imageName ."
+            script.sh "echo '${script.PASS}' | docker login -u '${script.USER}' --password-stdin"
+            script.sh "docker push shrey7781/$imageName"
+        }
+    }
+}
