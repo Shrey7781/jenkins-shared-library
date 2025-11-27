c42d4a121221ff221dfa6c0ac9a1ea1fdff42c66
Shrey7781 <shreeyansh.saxena.dev@gmail.com>
2025-11-27 12:25:45 +0530

    adding functions in Docker class

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,5 @@
-#!/usr/bin/env groovy
+#!/user/bin/env groovy
+import com.example.Docker
 def call(String imageName){
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-        echo "DEBUG - imageName received: ${imageName}"
-        sh "docker build -t ${imageName} ."
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh "docker push ${imageName}"
-}
+    return new Docker(this).buildDockerImage(imageName)
 }
